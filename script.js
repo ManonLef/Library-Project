9e2faa12844242f1b1a348d8b53bc443e762198c
Manon <manonlefkos@gmail.com>
2023-02-21 23:40:05 +0100

    add properties for the Book Object constructor

---
+++ script.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  // constructor with the book properties
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary() {
+
+}
